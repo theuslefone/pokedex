3e569bf2d1a84e6cd297432ac13444e140424b89
theuslefone <matheuskeeven22@gmail.com>
2023-09-27 15:23:24 -0300

    Configura style do input

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -7,8 +7,17 @@ interface SearchProps {
 const Search: React.FC<SearchProps> = ({ onChange }) => {
   return (
     <Input 
-      placeholder="Buscar Pokémon..."
-      onChange={onChange}
+    placeholder="Buscar Pokémon..."
+    onChange={onChange}
+    w="80%"
+    maxW="600px"
+    backgroundColor="white"
+    borderColor="gray.300"
+    borderWidth="1px"
+    borderRadius="md"
+    _focus={{ borderColor: 'gray.500', boxShadow: '0 0 0 1px gray.500' }}
+    _hover={{ borderColor: 'gray.400' }}
+    _placeholder={{ color: 'gray.400' }}
     />
   );
 }
