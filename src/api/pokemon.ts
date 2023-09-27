c5a8b99166365eb2c50f14b51ba1f60e024cd5a0
theuslefone <matheuskeeven22@gmail.com>
2023-09-27 11:59:05 -0300

    Refatora chamadas APÌ

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -6,8 +6,20 @@ export const fetchAllPokemons = async (limit = 150) => {
   return data.results.map((pokemon: any) => ({
     ...pokemon,
     id: parseInt(pokemon.url.split("/")[6], 10)
-    }));
+  }));
+};
+
+export const fetchPokemonDetails = async (pokemonUrl: string): Promise<PokemonDetails> => {
+  const response = await fetch(pokemonUrl);
+  const data: PokemonDetails = await response.json();
+  return data;
 };
 
 export const getPokemonImageURL = (id: number): string => 
   `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+export const getUniqueTypes = (pokemons: PokemonDetails[]): string[] => {
+  return Array.from(
+    new Set(pokemons.flatMap(pokemon => pokemon.types.map(typeInfo => typeInfo.type.name)))
+  );
+};
