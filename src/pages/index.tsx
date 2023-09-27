0bc9bc12e1fc80a8829d183d78888e601219ca17
theuslefone <matheuskeeven22@gmail.com>
2023-09-27 16:12:51 -0300

    Define melhor a estrutura e o estilo dos elementos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useState } from 'react';
 import Search from '../components/Search';
 import PokemonTabs from '../components/PokemonTabs';
 import PokemonSearchResults from '../components/PokemonSearchResults';
+import { Box, Image, Text, Flex, Spacer } from '@chakra-ui/react';
 
 export default function Home() {
   const [query, setQuery] = useState<string>('');
@@ -13,12 +14,25 @@ export default function Home() {
 
   return (
     <>
-      <Search onChange={handleSearchChange} />
-      {query ? (
-        <PokemonSearchResults query={query} />
-      ) : (
-        <PokemonTabs />
-      )}
+      <Box bgImage="url('/bg-red.svg')" h="650px">
+        <Flex alignItems="center" justifyContent='center'>
+          <Image src='/logo_pokemon.svg' alt="Logo Pokemon" w='350px' mb='15px' />
+        </Flex>
+
+        <Flex flexDirection="column" alignItems="center">
+          <Image w="500px" src='/img-pokeball.png' alt="Logo Pokemon" />
+          <Search onChange={handleSearchChange}/>
+          <Text pt='40px' fontSize='20px' color='white'>Comece digitando para buscar seu Pokémon favorito!</Text>
+        </Flex>
+      </Box>
+    
+      <Box mt='30px' maxW='1150px' m='20px auto'>
+        {query ? (
+          <PokemonSearchResults query={query} />
+        ) : (
+          <PokemonTabs />
+        )}
+      </Box>
     </>
-  )
+  );
 }
