26cfce4f02863c7464b69be55e92c85138713a83
theuslefone <matheuskeeven22@gmail.com>
2023-09-27 12:26:15 -0300

    Muda link para dentro da box

--- src/components/Pokemon/PokemonCard.tsx
+++ src/components/Pokemon/PokemonCard.tsx
@@ -1,15 +1,19 @@
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Box, Image, Link, Text } from '@chakra-ui/react';
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ id, name, imageUrl }) => {
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Image src={imageUrl} alt={`${name} image`} />
-      <Box p="6">
-        <Text as="h2" fontWeight="bold" fontSize="xl">
-          {name}
-        </Text>
-      </Box>
-    </Box>
+
+        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+        <Link href={`/pokemon/${id}`}>
+          <Image src={imageUrl} alt={`${name} image`} />
+          <Box p="6">
+            <Text as="h2" fontWeight="bold" fontSize="xl">
+            {name}
+            </Text>
+          </Box>
+        </Link>
+        </Box>
+
   );
 }
 
